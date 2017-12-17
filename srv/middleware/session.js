ec29e9d7002a67fc49e2026c0dcbfc96decdcf3d
Pitter <snider@degiant.com.br>
2017-12-16 22:33:16 -0200

    Sistema de Sessão agora utiliza pool de mysql

--- srv/middleware/session.js
+++ srv/middleware/session.js
@@ -1,13 +1,25 @@
 'use strict';
+const mysql = require('mysql2');
 const expressSession = require('express-session');
+const MySQLStore = require('express-mysql-session')(expressSession);
 
 const session = function () {
+
+  const pool = mysql.createPool({
+    connectionLimit: 5,
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'root',
+    database: 'inschool',
+    charset: 'utf8mb4_unicode_ci',
+  });
 
   const sessionConfig = {
     secret: '338@laksAKj181-2-a9381653@a!',
     resave: true,
     rolling: true,
-    saveUninitialized: true
+    saveUninitialized: false,
+    store: new MySQLStore({}, pool)
   };
 
   return expressSession(sessionConfig);
